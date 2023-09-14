55467c1ed70d0917b12d2efce8c47b2664418c79
Akaash Gupta <akaashgupta2005@gmail.com>
2023-09-14 17:50:19 +0530

    Student Main Page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignupDefaultPage from "./pages/auth/Signup";
 import Header from "./pages/components/header";
 import SubjectCard from "./pages/components/subject";
+import SidePanel from "./pages/components/sidePanel";
+import "./Routes.scss";
 
 const ProjectRoutes = () => {
   return (
     <React.Suspense fallback={<>Loading...</>}>
       <Router>
         <Routes>
-          <Route path="/" element={<><Header /> <SubjectCard/></>} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <div className="main-content">
+                  <SidePanel />
+                  <div className="subject-cards">
+                    <SubjectCard />
+                    <SubjectCard />
+                    <SubjectCard />
+                    <SubjectCard />
+                    <SubjectCard />
+                    {/* Add more SubjectCard components as needed */}
+                  </div>
+                </div>
+              </>
+            }
+          />
         </Routes>
       </Router>
     </React.Suspense>
---
+++ src/pages/components/sidePanel.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./sidePanel.scss"; // Import your SCSS file
+
+function SidePanel() {
+  return (
+    <div className="side-panel">
+      <div className="profile">
+        <img src="profile-picture.jpg" alt="Student Profile Picture" className="profile-picture" />
+        <h2 className="profile-name">John Doe</h2>
+      </div>
+      <div className="sections">
+        <div className="section">
+          <h3>Student Profile</h3>
+          {/* Add content for the Student Profile section here */}
+        </div>
+        <div className="section">
+          <h3>Courses Enrolled In</h3>
+          {/* Add content for the Courses Enrolled In section here */}
+        </div>
+        <div className="section">
+          <h3>Quiz Taken</h3>
+          {/* Add content for the Quiz Taken section here */}
+        </div>
+        <div className="section">
+          <h3>Leadership Board</h3>
+          {/* Add content for the Leadership Board section here */}
+        </div>
+        <div className="section">
+          <h3>Help Desk</h3>
+          {/* Add content for the Help Desk section here */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SidePanel;
